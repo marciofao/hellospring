d18eee3caa10e2ccc49556cadca7aad7f85b69cd
Marcio Fao <marcio.lopes.fao@gmail.com>
2026-07-14 21:59:24 -0300

    initialize hello world

---
+++ src/main/java/io/github/marciofao/hellospring/controller/HelloController.java
@@ -0,0 +1,15 @@
+package io.github.marciofao.hellospring.controller;
+
+import io.github.marciofao.hellospring.model.Produto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello Spring Boot!";
+    }
+}
